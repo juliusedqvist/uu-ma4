880c01832500277b79a31f143423fe8ceec7bebc
Julius Edqvist <julius.edqvist@gmail.com>
2023-09-30 00:36:35 +0200

    1

--- person.py
+++ person.py
@@ -4,16 +4,14 @@ import ctypes
 lib = ctypes.cdll.LoadLibrary('./libperson.so')
 
 
-def fib():
-    return lib.Person_test()
-
-
 class Person(object):
     def __init__(self, age):
         lib.Person_new.argtypes = [ctypes.c_int]
         lib.Person_new.restype = ctypes.c_void_p
         lib.Person_get.argtypes = [ctypes.c_void_p]
         lib.Person_get.restype = ctypes.c_int
+        lib.Person_test.argtypes = [ctypes.c_void_p]
+        lib.Person_test.restype = ctypes.c_int
         lib.Person_set.argtypes = [ctypes.c_void_p, ctypes.c_int]
         lib.Person_delete.argtypes = [ctypes.c_void_p]
         self.obj = lib.Person_new(age)
@@ -23,6 +21,9 @@ class Person(object):
 
     def set(self, age):
         lib.Person_set(self.obj, age)
+
+    def fib(self, age):
+        return lib.Person_test(self.obj)
 
     def __del__(self):
         return lib.Person_delete(self.obj)
