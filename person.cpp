08f150866b880c82bebafce1f9759216341c7f8f
Julius Edqvist <julius.edqvist@gmail.com>
2023-09-30 01:03:29 +0200

    1

--- person.cpp
+++ person.cpp
@@ -25,20 +25,10 @@ void Person::set(int n){
 	}
 
 int Person::fib(int n) {
-    if (n <= 0) {
-        return 0;
-    } else if (n == 1) {
-        return 1;
+    if (n <= 1) {
+        return n;
     } else {
-        int a = 0;
-        int b = 1;
-        int fibNum;
-        for (int i = 2; i <= n; i++) {
-            fibNum = a + b;
-            a = b;
-            b = fibNum;
-        }
-        return fibNum;
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
 
