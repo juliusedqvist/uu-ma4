662fc1db992fcafd3b019271cc8584d89c22fd6d
Julius Edqvist <julius.edqvist@gmail.com>
2023-10-08 10:41:47 +0200

    new

--- MA4_2.py
+++ MA4_2.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
-from person import Person
+# from person import Person
 from numba import njit
 from time import perf_counter as pc
-import concurrent.futures as future
 
 
 def fib_python(n):
@@ -21,20 +20,17 @@ def fib_numba(n):
         return fib_numba(n - 1) + fib_numba(n - 2)
 
 
-
-def runner(n):
-    return n
-
-
 def main():
+
     f = Person(1)
     tid_c = []
-    for i in range(30,40):
+    for i in range(1, 5):
         start = pc()
         f.set(i)
         f.fib()
         end = pc()
         tid_c.append(end-start)
+
 
     tid_numba = []
     for i in range(30, 40):
@@ -50,7 +46,7 @@ def main():
         end = pc()
         tid_python.append(end - start)
 
-    print(f"tid c: {tid_c}")
+    # print(f"tid c: {tid_c}")
     print(f"tid numba: {tid_numba}")
     print(f"tid python: {tid_python}")
 
